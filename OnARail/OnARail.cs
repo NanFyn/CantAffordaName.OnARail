90188e8bbf110a96e137ba7b2eb90ea30b1a389c
Big-Engine <71451068+Big-Engine@users.noreply.github.com>
2025-07-08 04:36:47 -0400

    Initial Idea
    A common joke for OW Modding is to make the planets move on rails instead of simulating orbits. Why not make a literal system on rails?

---
+++ OnARail/OnARail.cs
@@ -0,0 +1,92 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+using NewHorizons.Utility;
+using UnityEngine;
+using NewHorizons;
+using OWML.ModHelper.Events;
+
+namespace OnARail
+{
+    public class OnARail : ModBehaviour
+    {
+        private void Awake()
+        {
+            // You won't be able to access OWML's mod helper in Awake. Use Start() instead.
+            // Harmony Patches can go here.
+        }
+
+        internal void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(OnARail)} is loaded!", MessageType.Success);
+
+            // Get the New Horizons API and load configs
+            var newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            newHorizons.LoadConfigs(this);
+
+            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            {
+                if (loadScene != OWScene.SolarSystem) return;
+                ModHelper.Console.WriteLine("Loaded into solar system with On A Rail!", MessageType.Success);
+            };
+
+            newHorizons.GetStarSystemLoadedEvent().AddListener(OnStarSystemLoaded);
+        }
+
+        private void OnStarSystemLoaded(string systemName)
+        {
+            var newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+
+            GameObject oceanPlanet = newHorizons.GetPlanet("Ocean");
+            if (oceanPlanet != null)
+            {
+                GameObject water = SearchUtilities.Find("Ocean_Body/Sector/Water");
+                if (water != null)
+                {
+                    water.layer = LayerMask.NameToLayer("IgnoreSun");
+                    ModHelper.Console.WriteLine("Set layer to IgnoreSun in Ocean_Body/Sector/Water!", MessageType.Info);
+
+                    Material[] waterMaterial = water.GetComponent<TessellatedSphereRenderer>().GetComponent<TessellatedRenderer>()._materials;
+                    waterMaterial[1].color = new Color(0.8f, 0.8f, 1f, 0f);
+                    /*foreach (Material material in test)
+                    {
+                        if (material.name == "Ocean_GD_Surface_mat")
+                        {
+                            ModHelper.Console.WriteLine("Found it!", MessageType.Success);
+                            material.color = new Color(0f, 0f, 0f, 0f);
+                        }
+                        else
+                        {
+                            ModHelper.Console.WriteLine("Nope, sorry!", MessageType.Info);
+                        }
+                    }*/
+                }
+                else{ModHelper.Console.WriteLine("Can't find Ocean_Body/Sector/Water!", MessageType.Error);}
+                /*TessSphereSectorToggle oceanGDTessSectorToggle = SearchUtilities.Find("Ocean_Body/Sector/Ocean_GD").GetComponent<TessSphereSectorToggle>();
+                if (oceanGDTessSectorToggle != null)
+                {
+                    Destroy(oceanGDTessSectorToggle);
+                    ModHelper.Console.WriteLine("Destroying TessSphereSectorToggle in Ocean_GD!", MessageType.Info);
+                }
+                else
+                {
+                    ModHelper.Console.WriteLine("Can't find TessSphereSectorToggle in Ocean_GD!", MessageType.Error);
+                }
+                GameObject waterVolume = SearchUtilities.Find("Ocean_Body/Sector/WaterVolume");
+                if (waterVolume != null)
+                {
+                    RadialFluidVolume radialFluidVolume = waterVolume.GetComponent<RadialFluidVolume>();
+                    radialFluidVolume._buoyancyDensity = 0.9f;
+                }
+                else
+                {
+                    ModHelper.Console.WriteLine("Can't find WaterVolume!", MessageType.Error);
+                }*/
+            }
+            else
+            {
+                ModHelper.Console.WriteLine("Couldn't locate planet: Ocean!", MessageType.Error);
+            }
+        }
+    }
+}

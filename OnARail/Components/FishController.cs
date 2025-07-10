b98fbae6bd3aabd77dde83d30f20083b936df213
Big-Engine <71451068+Big-Engine@users.noreply.github.com>
2025-07-10 05:04:02 -0400

    Fresh Feesh
    Adds FishController with basic collision detection. Prototype for puzzle.

---
+++ OnARail/Components/FishController.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+
+namespace OnARail.Components
+{
+    public class FishController : MonoBehaviour
+    {
+        [Header("Rotation Timing")]
+        public AnimationCurve easingCurve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+        public float minRotationDuration = 15f;
+        public float maxRotationDuration = 30f;
+        public float endAngleThreshold = 2f;
+
+        [Header("Fishy Settings")]
+        public Transform fishTransform;
+        public float fishRotationSpeed = 6f;
+        public float fishDirectionSpeed = 6f;
+        public float minY = 41f;
+        public float maxY = 49f;
+        public float heightLerpSpeed = 0.5f;
+
+        [Header("Obstacle Detection")]
+        public LayerMask obstacleLayers = 1 << 12;
+        public float obstacleCheckDistance = 5f;
+        public float obstacleCheckCooldown = 0.5f;
+
+        private Quaternion startRotation;
+        private Quaternion targetRotation;
+        private float rotationDuration;
+        private float rotationTimer;
+        private float obstacleLastCheckTime = -1f;
+
+        private Vector3 fishDirectionSmoothed;
+        private Quaternion fishRotationSmoothed;
+        private Vector3 previousFishPosition;
+        private Vector3 previousOriginPosition;
+        private float targetYPosition;
+
+        void Awake()
+        {
+            fishTransform = transform.GetChild(0);
+        }
+
+        void Start()
+        {
+            previousFishPosition = fishTransform.position;
+            previousOriginPosition = transform.position;
+            fishDirectionSmoothed = fishTransform.forward;
+            fishRotationSmoothed = fishTransform.rotation;
+            targetYPosition = fishTransform.localPosition.y;
+
+            PickNewRotation();
+        }
+
+        void Update()
+        {
+            RotateParent();
+            RotateFish();
+            AdjustFishHeight();
+            ObstacleDetection();
+        }
+
+        void RotateParent()
+        {
+            rotationTimer += Time.deltaTime;
+            transform.rotation = Quaternion.Slerp(startRotation, targetRotation, easingCurve.Evaluate(Mathf.Clamp01(rotationTimer / rotationDuration)));
+
+            if (Quaternion.Angle(transform.rotation, targetRotation) < endAngleThreshold)
+            {
+                PickNewRotation();
+            }
+        }
+
+        void RotateFish()
+        {
+            Vector3 adjustedMovement = (fishTransform.position - previousFishPosition) - (transform.position - previousOriginPosition);
+            Vector3 movementDir = adjustedMovement.normalized;
+
+            if (movementDir.sqrMagnitude > 0.0001f)
+            {
+                fishDirectionSmoothed = Vector3.Slerp(fishDirectionSmoothed, movementDir, Time.deltaTime * fishDirectionSpeed);
+                fishRotationSmoothed = Quaternion.Slerp(fishRotationSmoothed, Quaternion.LookRotation(fishDirectionSmoothed, transform.up), Time.deltaTime * fishRotationSpeed);
+                fishTransform.rotation = fishRotationSmoothed;
+            }
+
+            previousFishPosition = fishTransform.position;
+            previousOriginPosition = transform.position;
+        }
+
+        void AdjustFishHeight()
+        {
+            Vector3 localPos = fishTransform.localPosition;
+            localPos.y = Mathf.Lerp(localPos.y, targetYPosition, Time.deltaTime * heightLerpSpeed);
+            fishTransform.localPosition = localPos;
+        }
+
+        void ObstacleDetection()
+        {
+            if (Time.time - obstacleLastCheckTime < obstacleCheckCooldown) return;
+
+            Vector3 origin = fishTransform.position;
+            Vector3 direction = fishDirectionSmoothed.normalized;
+
+            if (Physics.Raycast(origin, direction, out RaycastHit hit, obstacleCheckDistance, obstacleLayers))
+            {
+                obstacleLastCheckTime = Time.time;
+                PickNewRotation();
+            }
+        }
+
+        public void PickNewRotation()
+        {
+            startRotation = transform.rotation;
+            targetRotation = Random.rotation;
+            rotationDuration = Random.Range(minRotationDuration, maxRotationDuration);
+            rotationTimer = 0f;
+
+            fishDirectionSmoothed = fishTransform.forward;
+            targetYPosition = Random.Range(minY, maxY);
+        }
+    }
+}
--- OnARail/OnARail.cs
+++ OnARail/OnARail.cs
@@ -4,6 +4,9 @@ using NewHorizons.Utility;
 using UnityEngine;
 using NewHorizons;
 using OWML.ModHelper.Events;
+using OnARail.Components;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace OnARail
 {
@@ -43,35 +46,27 @@ namespace OnARail
                 GameObject water = SearchUtilities.Find("Ocean_Body/Sector/Water");
                 if (water != null)
                 {
-                    water.layer = LayerMask.NameToLayer("IgnoreSun");
-                    ModHelper.Console.WriteLine("Set layer to IgnoreSun in Ocean_Body/Sector/Water!", MessageType.Info);
+                    //water.layer = LayerMask.NameToLayer("IgnoreSun");
+                    //ModHelper.Console.WriteLine("Set layer to IgnoreSun in Ocean_Body/Sector/Water!", MessageType.Info);
 
                     Material[] waterMaterial = water.GetComponent<TessellatedSphereRenderer>().GetComponent<TessellatedRenderer>()._materials;
                     waterMaterial[1].color = new Color(0.4f, 0.8f, 1f, 0f);
-                    //Makes atmosphere render infront as it should, but not worth the hassle of reconfiguring renderQueue for everything
-                    //waterMaterial[1].renderQueue = 999;
                 }
                 else{ModHelper.Console.WriteLine("Can't find Ocean_Body/Sector/Water!", MessageType.Error);}
-                /*TessSphereSectorToggle oceanGDTessSectorToggle = SearchUtilities.Find("Ocean_Body/Sector/Ocean_GD").GetComponent<TessSphereSectorToggle>();
-                if (oceanGDTessSectorToggle != null)
+
+                GameObject fishStandardRoot = SearchUtilities.Find("Ocean_Body/Sector/Fish_Standard");
+                if (fishStandardRoot != null)
                 {
-                    Destroy(oceanGDTessSectorToggle);
-                    ModHelper.Console.WriteLine("Destroying TessSphereSectorToggle in Ocean_GD!", MessageType.Info);
+                    List<GameObject> fishList = SearchUtilities.GetAllChildren(fishStandardRoot);
+                    GameObject[] fish = fishList.ToArray();
+                    
+                    foreach (GameObject currentFish in fishList)
+                    {
+                        _ = currentFish.AddComponent<FishController>();
+                        ModHelper.Console.WriteLine("Added FishController to " + currentFish, MessageType.Info);
+                    }
                 }
-                else
-                {
-                    ModHelper.Console.WriteLine("Can't find TessSphereSectorToggle in Ocean_GD!", MessageType.Error);
-                }
-                GameObject waterVolume = SearchUtilities.Find("Ocean_Body/Sector/WaterVolume");
-                if (waterVolume != null)
-                {
-                    RadialFluidVolume radialFluidVolume = waterVolume.GetComponent<RadialFluidVolume>();
-                    radialFluidVolume._buoyancyDensity = 0.9f;
-                }
-                else
-                {
-                    ModHelper.Console.WriteLine("Can't find WaterVolume!", MessageType.Error);
-                }*/
+                else{ModHelper.Console.WriteLine("Can't find Fish_Standard!", MessageType.Error);}
             }
             else
             {
